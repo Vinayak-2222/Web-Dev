daff244c26ef7c943fb51adb5c24c5bcba6c68d7
Vinayak <officialvinayakst@gmail.com>
2025-11-19 18:52:06 +0530

    creating an element

---
+++ More on Dom project/script.js
@@ -0,0 +1,29 @@
+//create an element
+//appending child concept
+
+var btn=document.querySelector('button')
+var main=document.querySelector('main')
+var arr = [
+  "Stop right there!",
+  "Hands where I can see them!",
+  "Do not move!",
+  "Leave the area now!",
+  "This is your final warning!"
+];
+btn.addEventListener('click',function(){
+    var h1=document.createElement('h1')
+    var x=Math.random()*80
+    var y=Math.random()*80
+    var rot=Math.random()*360
+    var scl=Math.random()*3
+    var a=Math.floor(Math.random()*arr.length)
+
+    h1.innerHTML=arr[a]
+    h1.style.position='absolute'
+    h1.style.left=x+'%'
+    h1.style.top=y+'%'
+    h1.style.rotate=rot+'deg'
+    h1.style.scale=scl
+
+    main.appendChild(h1)
+})
